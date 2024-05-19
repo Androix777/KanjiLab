e92cd2c63980de4306ba69610595b2f34469c9ad
Androix777 <androix77@gmail.com>
2024-05-19 05:10:59 +0500

    Added daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import DatabaseService from "$lib/databaseService";
-	import * as wanakana from 'wanakana';
+	import * as wanakana from "wanakana";
+	import { themeChange } from 'theme-change';
+    import ThemeSelect from "$lib/components/ThemeSelect.svelte";
 
 	let inputElement: HTMLInputElement;
 
@@ -56,24 +58,53 @@
 		inputElement.focus();
 		wanakana.bind(inputElement);
 		void generateWord();
+		themeChange(false);
 	});
 </script>
 
-<h1>{word}</h1>
-<div>
-	<input bind:value={readingInput} onkeydown={checkWord} bind:this={inputElement}
-	style="
-		font-family: 'Yu Gothic UI';
-		font-size: 24px;
-		padding: 10px;
-		width: 75%;"/>
+<div class="absolute top-0 left-0 p-4">
+	<ThemeSelect/>
 </div>
-<div>
-	{answerStatus}
-</div>
-<div>
-	{previousWord}
-</div>
-<div>
-	{previousReadings.join(` `)}
+
+<div class="flex flex-col min-h-screen">
+    <!-- Верхняя половина -->
+    <div class="flex items-center justify-center flex-grow bg-base-200">
+        <div class="text-7xl">
+            {word}
+        </div>
+    </div>
+
+    <!-- Нижняя половина -->
+    <div class="flex flex-col items-center justify-center flex-grow space-y-4 bg-base-100 p-4">
+        <div class="w-full flex justify-center">
+            <input
+                bind:value={readingInput}
+                onkeydown={checkWord}
+                bind:this={inputElement}
+                class="input input-bordered input-lg text-center w-3/4 text-3xl"
+            />
+        </div>
+
+        <div class="w-full flex justify-center">
+            <div class="card w-3/4 bg-base-200 p-4">
+                <div class="flex flex-col space-y-2 text-xl">
+                    <div class="flex justify-center">
+                        {#if answerStatus}
+                            {answerStatus}
+                        {:else}
+                            <div class="opacity-0">Placeholder</div>
+                        {/if}
+                    </div>
+
+                    <div class="flex justify-center">
+                        {#if previousWord}
+                            {previousWord} - {previousReadings.join(`   `)}
+                        {:else}
+                            <div class="opacity-0">Placeholder</div>
+                        {/if}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
