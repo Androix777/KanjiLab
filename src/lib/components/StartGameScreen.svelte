842c1126002f85db8e9f3dc2e309f54f0f488061
Airplane <letadllo@gmail.com>
2024-07-06 19:10:57 +0500

    Start game screen

---
+++ src/lib/components/StartGameScreen.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+		import { LAUNCH_SERVER } from "$lib/tauriFunctions";
+		import { invoke } from "@tauri-apps/api/core";
+
+		async function launchServer()
+		{
+			await invoke(LAUNCH_SERVER);
+		}
+
+</script>
+
+<div class="h-screen flex flex-col">
+
+	<div class="flex-none">
+		<div class="flex justify-between h-12">
+			<div class="w-1/5 border text-center">
+				<button class="btn btn-primary w-full h-full rounded-none" onclick={() => { void launchServer(); }}>Host Game</button>
+			</div>
+			<div class="w-1/5 border text-center">
+				<button class="btn btn-primary w-full h-full rounded-none" onclick={() => {}}>Join Game</button>
+			</div>
+			<div class="w-3/5 border text-center">
+				<input class="input input-bordered text-center w-full h-full"/>
+			</div>
+		</div>
+	</div>
+
+	<div class="flex-grow flex">
+		<div class="w-3/5 border text-center">
+			<div class="flex flex-col justify-center h-full">
+				<span class="text-4xl">НАСТРОЙКИ</span>
+			</div>
+		</div>
+		<div class="w-2/5 flex flex-col">
+			<div class="flex-1 border text-center">Игроки</div>
+			<div class="flex-1 border text-center flex flex-col">
+				<div class="flex-grow flex flex-col">
+					<div> test message</div>
+					<div> test message</div>
+				</div>
+				<div class="flex justify-between h-12">
+					<input class="input input-bordered text-center w-full"/>
+					<button class="btn btn-primary w-12 rounded-none" onclick={() => {}}>Send</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="flex-none text-center h-12">
+		<div class="border-x">
+			<button class="btn btn-success w-full h-full rounded-none" onclick={() => {}}>START</button>
+		</div>
+	</div>
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,11 @@
     import GameScreen from "$lib/components/GameScreen.svelte";
     import SettingsScreen from "$lib/components/SettingsScreen.svelte";
     import StatsScreen from "$lib/components/StatsScreen.svelte";
+	import StartGameScreen from "$lib/components/StartGameScreen.svelte";
 	import { FontLoader } from '$lib/fontLoader';
 	import { themeChange } from 'theme-change';
-	import { LAUNCH_SERVER } from "$lib/tauriFunctions";
-    import { invoke } from "@tauri-apps/api/core";
 
-	async function launchServer()
-	{
-		await invoke(LAUNCH_SERVER);
-	}
-
-	type ScreenType = `Game` | `Settings` | `Stats`;
+	type ScreenType = `GameTest` | `Settings` | `Stats` | `StartGame`;
 	let currentScreenType: ScreenType = $state(`Settings`);
 	let fontLoader: FontLoader = new FontLoader();
 
@@ -30,7 +24,6 @@
 
 	$effect(() =>
 	{
-		void launchServer();
 		themeChange(false);
 		void loadFonts();
 	});
@@ -39,18 +32,21 @@
 <div class="flex h-screen">
 	<div class="w-16 min-h-full text-center bg-base-300">
 		<ul class="">
-			<button class="btn btn-primary" onclick={() => { setScreen(`Game`); }}>G</button>
+			<button class="btn btn-primary" onclick={() => { setScreen(`GameTest`); }}>Gl</button>
 			<button class="btn btn-primary" onclick={() => { setScreen(`Stats`); }}>St</button>
 			<button class="btn btn-primary" onclick={() => { setScreen(`Settings`); }}>S</button>
+			<button class="btn btn-primary" onclick={() => { setScreen(`StartGame`); }}>G</button>
 		</ul>
 	</div>
 	<div class="flex-1 bg-base-100">
 		{#if currentScreenType === `Stats`}
 			<StatsScreen />
-		{:else if currentScreenType === `Game`}
+		{:else if currentScreenType === `GameTest`}
 			<GameScreen fontLoader = {fontLoader} />
 		{:else if currentScreenType === `Settings`}
 			<SettingsScreen />
+		{:else if currentScreenType === `StartGame`}
+			<StartGameScreen />
 		{/if}
 	</div>
 </div>
