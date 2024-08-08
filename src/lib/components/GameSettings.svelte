3fc5ba5d04681b613d43114c3c82f425297eac40
Airplane <letadllo@gmail.com>
2024-08-08 20:57:22 +0500

    Game setings design, new player indicator

--- src/lib/components/GameSettings.svelte
+++ src/lib/components/GameSettings.svelte
@@ -15,89 +15,100 @@
 
 <div class="flex flex-col flex-grow h-full">
 	<div class="overflow-y-auto">
-		<div class="label">
-			<span class="label-text">Min frequency</span>
-		</div>
-		<input
-			type="number"
-			step="1"
-			oninput={(event) =>
-			{
-				if (event.target instanceof HTMLInputElement && event.target.value)
+		<div class="card-title">Game settings</div>
+		<div class="flex flex-row mt-4">
+			<div class="flex-1 text-left my-auto">
+				Min frequency
+			</div>
+			<input
+				type="number"
+				step="1"
+				oninput={(event) =>
 				{
-					getSettings().minFrequency.set(parseInt(event.target.value));
-				}
-			}}
-			value={getSettings().minFrequency.get()}
-			class="input input-bordered w-full"
-			style="-webkit-appearance: none;"
-		/>
+					if (event.target instanceof HTMLInputElement && event.target.value)
+					{
+						getSettings().minFrequency.set(parseInt(event.target.value));
+					}
+				}}
+				value={getSettings().minFrequency.get()}
+				class="input input-bordered w-1/2 text-center input-sm"
+				style="-webkit-appearance: none;"
+			/>
+		</div>
 
-		<div class="label">
-			<span class="label-text">Max frequency</span>
-		</div>
-		<input
-			type="number"
-			step="1"
-			oninput={(event) =>
-			{
-				if (event.target instanceof HTMLInputElement)
+		<div class="flex flex-row mt-4">
+			<div class="flex-1 text-left my-auto">
+				Max frequency
+			</div>
+			<input
+				type="number"
+				step="1"
+				oninput={(event) =>
 				{
-					getSettings().maxFrequency.set(parseInt(event.target.value));
-				}
-			}}
-			value={getSettings().maxFrequency.get()}
-			class="input input-bordered w-full"
-		/>
+					if (event.target instanceof HTMLInputElement)
+					{
+						getSettings().maxFrequency.set(parseInt(event.target.value));
+					}
+				}}
+				value={getSettings().maxFrequency.get()}
+				class="input input-bordered w-1/2 text-center input-sm"
+			/>
+		</div>
 
-		<div class="label">
-			<span class="label-text">Word part</span>
-		</div>
-		<input
-			oninput={(event) =>
-			{
-				if (event.target instanceof HTMLInputElement)
+		<div class="flex flex-row mt-4">
+			<div class="flex-1 text-left my-auto">
+				Word part
+			</div>
+			<input
+				oninput={(event) =>
 				{
-					getSettings().wordPart.set(event.target.value);
-				}
-			}}
-			value={getSettings().wordPart.get()}
-			class="input input-bordered w-full"
-		/>
+					if (event.target instanceof HTMLInputElement)
+					{
+						getSettings().wordPart.set(event.target.value);
+					}
+				}}
+				value={getSettings().wordPart.get()}
+				class="input input-bordered w-1/2 text-center input-sm"
+			/>
+		</div>
 
-		<div class="label">
-			<span class="label-text">Round duration</span>
-		</div>
-		<input
-			type="number"
-			step="1"
-			oninput={(event) =>
-			{
-				if (event.target instanceof HTMLInputElement)
+		<div class="flex flex-row mt-4">
+			<div class="flex-1 text-left my-auto">
+				Round duration
+			</div>
+			<input
+				type="number"
+				step="1"
+				oninput={(event) =>
 				{
-					getSettings().roundDuration.set(parseInt(event.target.value));
-				}
-			}}
-			value={getSettings().roundDuration.get()}
-			class="input input-bordered w-full"
-		/>
+					if (event.target instanceof HTMLInputElement)
+					{
+						getSettings().roundDuration.set(parseInt(event.target.value));
+					}
+				}}
+				value={getSettings().roundDuration.get()}
+				class="input input-bordered w-1/2 text-center input-sm"
+			/>
+		</div>
 
-		<div class="label">
-			<span class="label-text">Rounds count</span>
-		</div>
-		<input
-			type="number"
-			step="1"
-			oninput={(event) =>
-			{
-				if (event.target instanceof HTMLInputElement)
+		<div class="flex flex-row mt-4">
+			<div class="flex-1 text-left my-auto">
+				Rounds count
+			</div>
+				<input
+				type="number"
+				step="1"
+				oninput={(event) =>
 				{
-					getSettings().roundsCount.set(parseInt(event.target.value));
-				}
-			}}
-			value={getSettings().roundsCount.get()}
-			class="input input-bordered w-full"
-		/>
+					if (event.target instanceof HTMLInputElement)
+					{
+						getSettings().roundsCount.set(parseInt(event.target.value));
+					}
+				}}
+				value={getSettings().roundsCount.get()}
+				class="input input-bordered w-1/2 text-center input-sm"
+			/>
+		</div>
 	</div>
 
 	<div class="text-center flex-grow relative min-h-16">
--- src/lib/components/PlayerListCard.svelte
+++ src/lib/components/PlayerListCard.svelte
@@ -39,25 +39,29 @@
 	});
 </script>
 
-<div class="m-1 border border-primary bg-base-200 relative" style="border-radius: var(--rounded-box, 1rem /* 16px */);">
-	<div class="absolute right-2 top-0 text-primary h-6 text-xs">{isMe ? `You` : ``}</div>
-	<div class="flex flex-row justify-start items-center overflow-x-hidden min-w-0">
-		<div class="flex-none w-12 font-bold text-lg text-base-content">
-			{score}
-		</div>
-		<div class="flex-grow font-semibold text-base text-primary}">
-			<span class="text-primary">{clientInfo.is_admin ? `♔ ` : ``}</span>{clientInfo.name}
-		</div>
-	</div>
-	<div class="flex flex-row justify-center items-center overflow-x-hidden min-w-0" style="border-bottom-right-radius: var(--rounded-box, 1rem); border-bottom-left-radius: var(--rounded-box, 1rem);">
-		{#each getLastAnswers() as roundHistory(roundHistory.question)}
-			<div class="w-1/2 bg-opacity-50 {roundHistory.answers.get(clientInfo.id)?.answerStatus == `Correct` ? `bg-success text-success-content` : roundHistory.answers.get(clientInfo.id)?.answerStatus == `Incorrect` ? `bg-error text-error-content` : roundHistory.answers.get(clientInfo.id)?.answer ? `bg-warning text-warning-content` : ``}"
-				animate:flip={{ duration: 200 }}
-				style="min-width: 50%;">
-				{roundHistory.answers.get(clientInfo.id)?.answer ? roundHistory.answers.get(clientInfo.id)?.answer : `\xa0`}
+<div class="indicator w-full">
+	{#if isMe}
+	<span class="indicator-item badge badge-primary mx-4 my-1">You</span>
+	{/if}
+	<div class="m-1 border border-primary bg-base-200 relative w-full" style="border-radius: var(--rounded-box, 1rem /* 16px */);">
+		<div class="flex flex-row justify-start items-center overflow-x-hidden min-w-0">
+			<div class="flex-none w-12 font-bold text-lg text-base-content">
+				{score}
 			</div>
-		{:else}
-			<div class="opacity-0">Placeholder</div>
-		{/each}
+			<div class="flex-grow font-semibold text-base text-primary}">
+				<span class="text-primary">{clientInfo.is_admin ? `♔ ` : ``}</span>{clientInfo.name}
+			</div>
+		</div>
+		<div class="flex flex-row justify-center items-center overflow-x-hidden min-w-0" style="border-bottom-right-radius: var(--rounded-box, 1rem); border-bottom-left-radius: var(--rounded-box, 1rem);">
+			{#each getLastAnswers() as roundHistory(roundHistory.question)}
+				<div class="w-1/2 bg-opacity-50 {roundHistory.answers.get(clientInfo.id)?.answerStatus == `Correct` ? `bg-success text-success-content` : roundHistory.answers.get(clientInfo.id)?.answerStatus == `Incorrect` ? `bg-error text-error-content` : roundHistory.answers.get(clientInfo.id)?.answer ? `bg-warning text-warning-content` : ``}"
+					animate:flip={{ duration: 200 }}
+					style="min-width: 50%;">
+					{roundHistory.answers.get(clientInfo.id)?.answer ? roundHistory.answers.get(clientInfo.id)?.answer : `\xa0`}
+				</div>
+			{:else}
+				<div class="opacity-0">Placeholder</div>
+			{/each}
+		</div>
 	</div>
 </div>
