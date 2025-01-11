7186067e7cf954a8421a3952ccb7f89a53466790
Androix777 <androix77@gmail.com>
2025-01-11 17:10:45 +0500

    Fixed linter

---
+++ eslint.config.mjs
@@ -0,0 +1,82 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import parser from "svelte-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+});
+
+export default [{
+	ignores: [
+		"kanji_lab_loader",
+		"**/.DS_Store",
+		"**/node_modules",
+		"build",
+		"src-tauri",
+		".svelte-kit",
+		"package",
+		"**/.env",
+		"**/.env.*",
+		"!**/.env.example",
+		"**/eslint.config.mjs",
+		"**/svelte.config.js",
+		"**/tailwind.config.cjs",
+		"**/postcss.config.js",
+		"**/pnpm-lock.yaml",
+		"**/package-lock.json",
+		"**/yarn.lock",
+	],
+}, ...compat.extends(
+	"eslint:recommended",
+	"plugin:@typescript-eslint/strict-type-checked",
+	"plugin:svelte/recommended",
+), {
+	plugins: {
+		"@typescript-eslint": typescriptEslint,
+	},
+
+	languageOptions: {
+		globals: {
+			...globals.browser,
+			...globals.node,
+		},
+
+		parser: tsParser,
+		ecmaVersion: 2020,
+		sourceType: "module",
+
+		parserOptions: {
+			extraFileExtensions: [".svelte"],
+			project: ["./tsconfig.json"],
+		},
+	},
+
+	rules: {
+		"@typescript-eslint/restrict-template-expressions": "off",
+
+		"@typescript-eslint/no-misused-promises": ["error", {
+			checksVoidReturn: false,
+		}],
+	},
+}, {
+	files: ["**/*.svelte"],
+
+	languageOptions: {
+		parser: parser,
+		ecmaVersion: 5,
+		sourceType: "script",
+
+		parserOptions: {
+			parser: "@typescript-eslint/parser",
+		},
+	},
+}];
